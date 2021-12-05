e702cb2b3dc720d6b0bbdaa90306bc60472cca33
nasifhasank <noyel.nhk@gmail.com>
2021-12-05 22:05:52 +0600

    new demo (updated version)

--- main.cpp
+++ main.cpp
@@ -15,11 +15,6 @@ void init()
    // glClearColor(0.0,0.0,0.0,1.0);
 }
 
-//opposite_car_move
-float y_positioncar1= 0.0;
-float y_positioncar2= 0.0;
-int state1=1;
-int state2=2;
 //main_car_move
 int maincar_pos_x = 0;
 int maincar_pos_y = 0;
@@ -44,11 +39,19 @@ void wheel()
 
 
 }
+//road_divider
 float divider1=0;
 float divider2=5;
 float divider3=-5;
 float divider4=-10;
 float divider5=-15;
+
+//opposite_car
+float car1=0;
+float car2=6;
+float car3=-5;
+float car4=0;
+
 
 void road_middle()
 {
@@ -87,30 +90,30 @@ void road_middle()
 
         glLoadIdentity();
 
-        divider1 = divider1 - 0.05;
+        divider1 = divider1 - 0.4;
            if (divider1 < -4.5)
         {
         divider1 = 1;
 
         }
-        divider2 = divider2 - 0.05;
+        divider2 = divider2 - 0.4;
            if (divider2 < +0.5)
         {
         divider2 = 6;
 
         }
-        divider3 = divider3 - 0.05;
+        divider3 = divider3 - 0.4;
            if (divider3 < -9.5)
         {
         divider3 = -4;
 
         }
-        divider4 = divider4 - 0.05;
+        divider4 = divider4 - 0.4;
            if (divider4 < -14.5)
         {
         divider4 = -9;
         }
-         divider5 = divider5 - 0.05;
+         divider5 = divider5 - 0.4;
            if (divider5 < -19.5)
         {
         divider5 = -14;
@@ -180,7 +183,20 @@ void start_game()
 
     ///main_car
    //  glTranslatef(maincar_pos_x+0.0,maincar_pos_y+0.0,0.0);
-    ///wheel();
+    glBegin(GL_QUADS);
+    // front
+    glColor3f(0.0,0.0,0.0);
+    glVertex2f(maincar_pos_x-3.3,maincar_pos_y-6.5);
+    glVertex2f(maincar_pos_x-3.3,maincar_pos_y-7);
+    glVertex2f(maincar_pos_x-1.3,maincar_pos_y-7);
+    glVertex2f(maincar_pos_x-1.3,maincar_pos_y-6.5);
+    //back
+     glColor3f(0.0,0.0,0.0);
+    glVertex2f(maincar_pos_x-3.3,maincar_pos_y-8.5);
+    glVertex2f(maincar_pos_x-3.3,maincar_pos_y-8);
+    glVertex2f(maincar_pos_x-1.3,maincar_pos_y-8);
+    glVertex2f(maincar_pos_x-1.3,maincar_pos_y-8.5);
+    glEnd();
     //car_body
     glBegin(GL_POLYGON);
     glColor3f(1.0,1.0,1.0);
@@ -195,30 +211,151 @@ void start_game()
     glEnd();
      glLoadIdentity();
 
-     ///opposite_car_1
-    // glTranslatef(5.0,y_positioncar1+10.0,0.0);
-     ///wheel();
+     ///opposite_car_1 (red)
+
+     //wheel();
+
+        glBegin(GL_QUADS);
+    // front
+    glColor3f(0.0,0.0,0.0);
+    glVertex2f(0.7,car1+13.5);
+    glVertex2f(0.7,car1+13);
+    glVertex2f(2.7,car1+13);
+    glVertex2f(2.7,car1+13.5);
+    //back
+     glColor3f(0.0,0.0,0.0);
+    glVertex2f(0.7,car1+12);
+    glVertex2f(0.7,car1+11.5);
+    glVertex2f(2.7,car1+11.5);
+    glVertex2f(2.7,car1+12);
+    glEnd();
+
      //car
     glBegin(GL_POLYGON);
     glColor3f(1.0,0.0,0.0);
-    glVertex2f(2,y_positioncar1 +4);
-    glVertex2f(2,y_positioncar1 +1);
-    glVertex2f(3.5,y_positioncar1 +1);
-    glVertex2f(3.5,y_positioncar1 +4);
+    glVertex2f(1,car1 +14);
+    glVertex2f(1,car1 +11);
+    glVertex2f(2.5,car1 +11);
+    glVertex2f(2.5, car1 +14);
 
     glEnd();
      glLoadIdentity();
 
-    ///opposite_car_2
-     glTranslatef(0.0,y_positioncar2+13.0,0.0);
-    ///wheel();
+      car1 = car1 - 0.2;
+         if (car1 < -30)
+       {
+        car1 = 1;
+          }
+
+    ///opposite_car_2 ( blue )
+
+    //wheel();
+
+     glBegin(GL_QUADS);
+    // front
+    glColor3f(0.0,0.0,0.0);
+
+    glVertex2f(-2.8,car2+6.5);
+    glVertex2f(-2.8,car2+6);
+    glVertex2f(-0.8,car2+6);
+    glVertex2f(-0.8,car2+6.5);
+    //back
+     glColor3f(0.0,0.0,0.0);
+    glVertex2f(-2.8,car2+5);
+    glVertex2f(-2.8,car2+4.5);
+    glVertex2f(-0.8,car2+4.5);
+    glVertex2f(-0.8,car2+5);
+    glEnd();
+
+
     //car
     glBegin(GL_POLYGON);
     glColor3f(0.0,0.0,1.0);
-    glVertex2f(-3,y_positioncar2 +7);
-    glVertex2f(-3,y_positioncar2 +4);
-    glVertex2f(-1.5,y_positioncar2 +4);
-    glVertex2f(-1.5,y_positioncar2 +7);
+    glVertex2f(-2.5,car2 +7);
+    glVertex2f(-2.5, car2+4);
+    glVertex2f(-1, car2+4);
+    glVertex2f(-1, car2+7);
+
+    glEnd();
+     glLoadIdentity();
+
+     car2 = car2 - 0.08;
+           if (car2 < -30)
+        {
+        car2 = 7;
+
+        }
+
+     ///opposite_car_3  ( cyan )
+
+    //wheel();
+
+    glBegin(GL_QUADS);
+    // front
+    glColor3f(0.0,0.0,0.0);
+    glVertex2f(-5.8,car3+9.5);
+    glVertex2f(-5.8,car3+9);
+    glVertex2f(-3.8,car3+9);
+    glVertex2f(-3.8,car3+9.5);
+    //back
+     glColor3f(0.0,0.0,0.0);
+    glVertex2f(-5.8,car3+8);
+    glVertex2f(-5.8,car3+7.5);
+    glVertex2f(-3.8,car3+7.5);
+    glVertex2f(-3.8,car3+8);
+    glEnd();
+
+    //car
+    glBegin(GL_POLYGON);
+    glColor3f(0.0,1.0,1.0);
+
+    glVertex2f(-5.5, car3+10);
+    glVertex2f(-5.5,car3 +7);
+    glVertex2f(-4, car3 +7);
+    glVertex2f(-4,car3 +10);
+
+    glEnd();
+     glLoadIdentity();
+
+     car3 = car3 - 0.1;
+        if (car3 < -30)
+       {
+        car3 = 3;
+
+       }
+
+    ///opposite_car_4 ( yellow )
+    ///wheel();
+
+      glBegin(GL_QUADS);
+    // front
+    glColor3f(0.0,0.0,0.0);
+    glVertex2f(3.7,car4+4.5);
+    glVertex2f(3.7,car4+4);
+    glVertex2f(5.7,car4+4);
+    glVertex2f(5.7,car4+4.5);
+    //back
+     glColor3f(0.0,0.0,0.0);
+    glVertex2f(3.7,car4+3);
+    glVertex2f(3.7,car4+2.5);
+    glVertex2f(5.7,car4+2.5);
+    glVertex2f(5.7,car4+3);
+    glEnd();
+
+    //car
+    glBegin(GL_POLYGON);
+    glColor3f(1.0,1.0,0.0);
+    glVertex2f(4, car4 +5);
+    glVertex2f(4, car4 +2);
+    glVertex2f(5.5,car4 +2);
+    glVertex2f(5.5,car4 +5);
+
+     car4 = car4 - 0.15;
+           if (car4 < -30)
+        {
+        car4 = 7;
+
+       }
 
     glEnd();
      glLoadIdentity();
@@ -232,7 +369,6 @@ void reshape(int w,int h)
         glLoadIdentity();
 
     gluOrtho2D(-10,10,-10,10);
-    //gluPerspective(60,1,2.0,50.0);
 
     glMatrixMode(GL_MODELVIEW);
 }
@@ -241,39 +377,6 @@ void timer(int)
 {
     glutPostRedisplay();
     glutTimerFunc(1000/60,timer,0);
-
-    switch(state1)
-    {
-
-    case -1:
-        if(y_positioncar1<10)
-            y_positioncar1+=0.15;
-        else
-            state1=1;
-        break;
-    case 1:
-        if(y_positioncar1>-13)
-            y_positioncar1-=0.08;
-        else
-            state1=-1;
-        break;
-    }
-    switch(state2)
-    {
-
-    case -2:
-        if(y_positioncar2 < 8)
-            y_positioncar2+=0.08;
-        else
-            state2=2;
-        break;
-    case 2:
-        if(y_positioncar2>-15)
-            y_positioncar2-=0.15;
-        else
-            state2=-2;
-        break;
-    }
 
 }
 
